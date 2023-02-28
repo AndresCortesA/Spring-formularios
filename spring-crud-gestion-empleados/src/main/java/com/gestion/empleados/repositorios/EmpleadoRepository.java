bc46c3fcab364385a8da4dbf1e9ab90bc4008a2b
AndresAriasCortes <101019474+AndresCortesA@users.noreply.github.com>
2023-02-27 20:22:01 -0500

    Delete spring-crud-gestion-empleados directory

--- spring-crud-gestion-empleados/src/main/java/com/gestion/empleados/repositorios/EmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestion.empleados.repositorios;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.gestion.empleados.entidades.Empleado;
-
-public interface EmpleadoRepository extends PagingAndSortingRepository<Empleado, Long> {
-
-}
